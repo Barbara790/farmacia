72664b28ac7117f9e500812c43ce436eff28cd16
Barbara790 <barbara2.dossantos@gmail.com>
2021-06-02 19:37:12 -0300

    Projeto farmacia

---
+++ src/main/java/com/generation/farmacia/controller/CategoriaController.java
@@ -0,0 +1,56 @@
+package com.generation.farmacia.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.farmacia.model.Categoria;
+import com.generation.farmacia.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+	
+	public CategoriaRepository repositorio;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> GetAll(){
+		return ResponseEntity.ok(repositorio.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> GetById(@PathVariable long id){
+		return repositorio.findById(id).map(resposta-> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/descricao/{descricao}")
+	public ResponseEntity<List<Categoria>> GetByDescricao(@PathVariable String descricao){
+		return ResponseEntity.ok(repositorio.findAllByDescricaoContainingIgnoreCase(descricao));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria>post(@RequestBody Categoria descricao){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repositorio.save(descricao));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria>put(@RequestBody Categoria descricao){
+		return ResponseEntity.status(HttpStatus.OK).body(repositorio.save(descricao));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete (@PathVariable long id) {
+		repositorio.deleteById(id);
+	}
+	
+
+}
